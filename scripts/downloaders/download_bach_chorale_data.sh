6d4932b1b2a827adb2c889719e986aaed17d7bff
anilegin <81387793+anilegin@users.noreply.github.com>
2024-07-05 00:59:26 +0200

    deletion of unneccessary folders

--- scripts/downloaders/download_bach_chorale_data.sh
+++
@@ -1,7 +0,0 @@
-docker compose exec app bash -c "
-mkdir /app/data/bach-chorales/
-cd /app/data/bach-chorales/ && 
-echo 'Downloading...' && 
-curl -LO https://github.com/czhuang/JSB-Chorales-dataset/raw/master/Jsb16thSeparated.npz -s &&
-echo '🚀 Done!'
-" 
